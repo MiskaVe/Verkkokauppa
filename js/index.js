f2ad2ab876cdbec6c04946984b46b5b12296aa52
MiskaVe <miska.venalainen998@gmail.com>
2024-11-20 14:46:20 +0200

    lisätty flexbox tuotteille ja ostokori sivu

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var ikkuna = window.location.href;
 
 if (ikkuna = "./index.html") {
-    document.getElementById("etusivu").style.padding = "2%";
+    document.getElementById("etusivu").style.padding = "3.2%";
     document.getElementById("etusivu").style.color = "white";
 };
--- js/yhteydenotto.js
+++ js/yhteydenotto.js
@@ -1,6 +1,6 @@
 var ikkuna = window.location.href;
 
 if (ikkuna = "./yhteydenotto.html") {
-    document.getElementById("yhteys").style.padding = "2%";
+    document.getElementById("yhteys").style.padding = "3.2%";
     document.getElementById("yhteys").style.color = "white";
 }
