4c1df0ec728d117d76dc2ad63e4017dbb4a39640
MiskaVe <miska.venalainen998@gmail.com>
2024-11-20 14:06:13 +0200

    paremmat linkit

---
+++ js/tietoa.js
@@ -0,0 +1,6 @@
+var ikkuna = window.location.href;
+
+if (ikkuna = "./tietoa.html") {
+    document.getElementById("tietoa").style.padding = "2%";
+    document.getElementById("tietoa").style.color = "white";
+}
--- yhteydenotto.js
+++
@@ -1,6 +0,0 @@
-var ikkuna = window.location.href;
-
-if (ikkuna = "C:\Users\miska.venalainen\OneDrive - Ita-Savon Koulutuskuntayhtyma\Ohjelmistokehittäjänä toimiminen\Verkkokauppa\html\yhteydenotto.html") {
-    document.getElementById("yhteydenotto").style.padding = "2%";
-    document.getElementById("yhteydenotto").style.color = "white";
-}
