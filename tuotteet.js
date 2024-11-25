b2b3e664b8054f51b38a5ebd0cc5bdc593586864
Miska Venäläinen <miska.venalainen998@gmail.com>
2024-11-25 08:16:51 +0200

    parit turhat js-tiedostot poistettu, sivun kannalta ei muutoksia

--- tuotteet.js
+++
@@ -1,8 +0,0 @@
-var ikkuna = window.location.href;
-
-alert(ikkuna);
-
-if (ikkuna = "C:/Users/miska.venalainen/OneDrive%20-%20Ita-Savon%20Koulutuskuntayhtyma/Ohjelmistokehitt%C3%A4j%C3%A4n%C3%A4%20toimiminen/Verkkokauppa/html/tuotteet.html") {
-    document.getElementById("etusivu").style.padding = "2%";
-    document.getElementById("etusivu").style.color = "white";
-};
